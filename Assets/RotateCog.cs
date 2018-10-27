5898ae9efa1901be38c7d7eebe0e01a5ac582c39
Avi <avinash.valsnt.persaud@gmail.com>
2018-10-26 20:59:27 -0400

    Rotating Big Cog

---
+++ Assets/RotateCog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCog : MonoBehaviour {
+    public float RotateSpeed = 2.5f;
+
+    public Transform Target;
+
+    private Vector3 zAxis = new Vector3(0, 0, 1);
+
+    private void Update()
+    {
+        this.transform.RotateAround(Target.position, zAxis, RotateSpeed);
+    }
+}
