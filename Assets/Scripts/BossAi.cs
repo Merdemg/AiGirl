8f5a150a80cf6f662bf6f995720e1f6be38cfeff
Avi <avinash.valsnt.persaud@gmail.com>
2018-10-27 21:36:06 -0400

    no message

--- Assets/Scripts/BossAi.cs
+++ Assets/Scripts/BossAi.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BossAi : MonoBehaviour {
-    public int Health = 10;   
-	// Use this for initialization
-	void Start () {
+
+    public int Health = 10;
+    float counter = 0;
+    [SerializeField] float shootCoolDown = 2.5f;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootSpeed = 1000;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (counter < shootCoolDown)
+        {
+            counter += Time.deltaTime;
+        }
+        else if (counter >= shootCoolDown)
+        {
+            counter = 0;
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
